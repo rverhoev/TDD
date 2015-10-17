e5bae0a81dfb5d1108041455edde7fb9c5346b8f
Robert Verhoeve <rverhoeve67@gmail.com>
2015-10-17 15:38:47 +0200

    aa

---
+++ lists/migrations/0005_item_list.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0004_remove_list_list'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='item',
+            name='list',
+            field=models.TextField(default=''),
+        ),
+    ]
