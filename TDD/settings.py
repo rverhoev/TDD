3185451135a08eb43bc8b2be4adb2fecb4c46bba
Robert Verhoeve <rverhoeve67@gmail.com>
2015-10-18 14:11:26 +0200

    Add static tag

--- TDD/settings.py
+++ TDD/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-#    'django.middleware.security.SecurityMiddleware',
+    #    'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'TDD.urls'
@@ -102,4 +102,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+# STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "../static"),
+    '/var/www/rverhoev/static/',
+)
