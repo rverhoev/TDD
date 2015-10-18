6ed3b9f985c1d89d59f49d4943d0d56c2b6ecd2b
Robert Verhoeve <rverhoeve67@gmail.com>
2015-10-18 14:14:53 +0200

    Add static tag

--- TDD/urls.py
+++ TDD/urls.py
@@ -21,4 +21,4 @@ urlpatterns = [
     url(r'^lists/', include('lists.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
